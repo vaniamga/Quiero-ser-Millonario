efe4dc7e46ce1199663fe94eec268747400ae044
Ana <135507348+AnaMartinezMix@users.noreply.github.com>
2023-07-13 08:55:01 -0600

    Preguntas

---
+++ funcion.js
@@ -0,0 +1,99 @@
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion1");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion2");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion3");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion4");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion5");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion6");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion7");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion8");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion9");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion10");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+function validarRespuestaUno(){
+    let opcion1 =document.getElementById("opcion11");
+    if(opcion1.cheched){
+        alert("Respuesta correcta");
+        let = document.getElementById();
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
