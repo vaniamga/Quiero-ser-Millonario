eb329758422703541dbffedacc3fc5f600bd6aa0
Dulce Velázquez <dulcekarinavb@outlook.com>
2023-07-17 21:38:16 -0600

    mi primer commit

--- funcion.js
+++ funcion.js
@@ -1,103 +1,12 @@
-function validarRespuestaUno(){
-    let opcion1 = document.getElementById("opcion1");
+function preguntaUno(){
+    let respuestaA= document.getElementById("respuestaA");
     let nextBtn = document.getElementById("next-btn");
-
-    if(opcion1.checked){
+    if(respuestaA.checked){
         alert("Respuesta correcta");
         nextBtn.disabled = false;
+        preguntaDos();
     }else{
         alert("Respuesta incorrecta");
     }  
 }
 
-
-function validarRespuestaDos(){
-    let opcion1 =document.getElementById("opcion2");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion3");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion4");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion5");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion6");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion7");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion8");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion9");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion10");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
-function validarRespuestaUno(){
-    let opcion1 =document.getElementById("opcion11");
-    if(opcion1.cheched){
-        alert("Respuesta correcta");
-        let = document.getElementById();
-    }else{
-        alert("Respuesta incorrecta");
-    }
-}
