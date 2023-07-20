071715608355fffe39d0126c29d813f207dc1938
Dulce Velázquez <dulcekarinavb@outlook.com>
2023-07-19 23:05:30 -0600

    mi segundo commit

--- funcion.js
+++ funcion.js
@@ -10,3 +10,15 @@ function preguntaUno(){
     }  
 }
 
+function preguntaDos(){
+    let respuestaA= document.getElementById("respuestaA");
+    let nextBtn = document.getElementById("next-btn");
+    if(respuestaA.checked){
+        alert("Respuesta correcta");
+        nextBtn.disabled = false;
+        preguntaDos();
+    }else{
+        alert("Respuesta incorrecta");
+    }  
+}
+
