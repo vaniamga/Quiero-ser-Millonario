53f33338c6fd6d007332e8617427ce6730ee4ff8
vaniamga <vaniagallardo578@gmail.com>
2023-07-21 01:59:53 -0600

    funcion

--- funcion.js
+++ funcion.js
@@ -1,24 +1,65 @@
-function preguntaUno(){
-    let respuestaA= document.getElementById("respuestaA");
-    let nextBtn = document.getElementById("next-btn");
-    if(respuestaA.checked){
-        alert("Respuesta correcta");
-        nextBtn.disabled = false;
-        preguntaDos();
-    }else{
-        alert("Respuesta incorrecta");
-    }  
-}
+function verificarRespuesta(preguntaId) {
+    const pregunta = document.getElementById(preguntaId);
+    const opciones = pregunta.querySelectorAll("input[type=radio]");
+    let respuestaCorrecta = false;
 
-function preguntaDos(){
-    let respuestaA= document.getElementById("respuestaA");
-    let nextBtn = document.getElementById("next-btn");
-    if(respuestaA.checked){
-        alert("Respuesta correcta");
-        nextBtn.disabled = false;
-        preguntaDos();
-    }else{
-        alert("Respuesta incorrecta");
-    }  
-}
+    opciones.forEach((opcion) => {
+        if (opcion.checked) {
+           
+            switch (preguntaId) {
+                case "pregunta1":
+                    respuestaCorrecta = opcion.value === "A";
+                    break;
+                case "pregunta2":
+                    respuestaCorrecta = opcion.value === "B";
+                    break;
+                
+                case "pregunta3":
+                    respuestaCorrecta = opcion.value === "A";
+                    break;
+                case "pregunta4":
+                    respuestaCorrecta = opcion.value === "A";
+                    break;
+                case "pregunta5":
+                        respuestaCorrecta = opcion.value === "A";
+                        break; 
+                case "pregunta6":
+                        respuestaCorrecta = opcion.value === "A";
+                        break;
+                case "pregunta7":
+                        respuestaCorrecta = opcion.value === "C";
+                        break;
+                case "pregunta8":
+                        respuestaCorrecta = opcion.value === "B";
+                        break; 
+                case "pregunta9":
+                        respuestaCorrecta = opcion.value === "B";
+                        break;
+                case "pregunta10":
+                        respuestaCorrecta = opcion.value === "C";
+                        break;                              
+                
+                default:
+                    respuestaCorrecta = false;
+            }
+        }
+    });
 
+    if (respuestaCorrecta) {
+        pregunta.style.display = "none";
+
+        // Mostrar la siguiente pregunta si existe
+        const siguientePreguntaId = "pregunta" + (parseInt(preguntaId.slice(8)) + 1);
+        const siguientePregunta = document.getElementById(siguientePreguntaId);
+        if (siguientePregunta) {
+            siguientePregunta.style.display = "block";
+            
+        } else {
+            
+            // Si no hay más preguntas, se puede mostrar un mensaje o realizar alguna acción final.
+            alert("¡Felicidades! has ganado 1 millon de pesos ");
+        }
+    } else {
+        alert("Respuesta incorrecta. Inténtalo de nuevo.");
+    }
+}
